1e9d2c0a110f3be0f2d2e0ef2b2a4deeb53fac8d
Aleexmarcelo <jojoanete@gmail.com>
2021-10-06 23:03:30 -0300

    Por algum motivo, o primeiro push não atualizou o site do git, testando novamente

--- Matriz/Program.cs
+++ Matriz/Program.cs
@@ -6,28 +6,31 @@ namespace Matriz
     {
         static void Main(string[] args)
         {
-            //Leitura dos numeros aplicados da primeira linha
+            //Leitura dos numeros aplicados para matriz quadrada
             int n = int.Parse(Console.ReadLine());
-            
+            //Criando a matriz usando N
             int[,] mat = new int[n,n];
-            //
+            //Criado um loop nas linhas da matriz
             for (int i = 0; i < n; i++)
             {
-                string[] values = Console.ReadLine().Split();
+                //Array para guardar valores
+                string[] values = Console.ReadLine().Split(' ');
 
+                //Criado um loop nas linhas da matriz
                 for (int j = 0; j < n; j++)
                 {
+                    //Aplicando os valores em seus devidos lugares
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
             Console.WriteLine("Main Diagonal:");
-
+            //Criado um loop nas linhas da matriz
             for (int i = 0; i < n; i++)
             {
                 Console.Write(mat[i,i] + "");
             }
             Console.WriteLine();
-
+            //Variavel que conta os numeros negativos e passa por todas as posicoes
             int count = 0;
             for (int i = 0; i < n; i++)
             {
